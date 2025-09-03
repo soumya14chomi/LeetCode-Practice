c35d1d4260d54ea2d0b0bea9f0b8a676a0d3235e
soumya14chomi <68002631+soumya14chomi@users.noreply.github.com>
2025-09-02 23:15:51 -0400

    Time: 7 ms (8.25%), Space: 44.8 MB (51.32%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,16 +1,20 @@
 class Solution {
+        static Map<Character, Integer> hm = new HashMap<>();
+        Solution(){
+            hm.put('I', 1);
+            hm.put('V', 5);
+            hm.put('X', 10);
+            hm.put('L', 50);
+            hm.put('C', 100);
+            hm.put('D', 500);
+            hm.put('M', 1000);
+        }
+
     public int romanToInt(String s) {
         Stack<Integer> st = new Stack<>();
         int n = s.length();
 
-        Map<Character, Integer> hm = new HashMap<>();
-        hm.put('I', 1);
-        hm.put('V', 5);
-        hm.put('X', 10);
-        hm.put('L', 50);
-        hm.put('C', 100);
-        hm.put('D', 500);
-        hm.put('M', 1000);
+
 
         int res=0;
         for(int i=0; i<n;i++){
