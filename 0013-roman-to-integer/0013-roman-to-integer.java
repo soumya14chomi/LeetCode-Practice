b9db1b17a44fbfe1cdf5374945bb5362ccd946e1
soumya14chomi <68002631+soumya14chomi@users.noreply.github.com>
2025-09-02 23:22:54 -0400

    Time: 5 ms (55.59%), Space: 44.8 MB (51.32%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,41 +1,22 @@
 class Solution {
-        static Map<Character, Integer> hm = new HashMap<>();
-        Solution(){
-            hm.put('I', 1);
-            hm.put('V', 5);
-            hm.put('X', 10);
-            hm.put('L', 50);
-            hm.put('C', 100);
-            hm.put('D', 500);
-            hm.put('M', 1000);
-        }
 
     public int romanToInt(String s) {
-        Stack<Integer> st = new Stack<>();
+        Map<Character, Integer> hm = new HashMap<>();
+        hm.put('I', 1);
+        hm.put('V', 5);
+        hm.put('X', 10);
+        hm.put('L', 50);
+        hm.put('C', 100);
+        hm.put('D', 500);
+        hm.put('M', 1000);
+        int res =0;
         int n = s.length();
-
-
-
-        int res=0;
-        for(int i=0; i<n;i++){
-            if(st.isEmpty())    st.push(hm.get(s.charAt(i)));
-            else if(st.peek() < hm.get(s.charAt(i))) {
-                int val = st.pop();
-                st.push(hm.get(s.charAt(i))-val);
-                
-
-            }
-            else {
-                st.push(hm.get(s.charAt(i)));
-            }
-
-            // System.out.println(st.toString());
+        for(int i=0; i< n-1;i++){
+            if(hm.get(s.charAt(i)) < hm.get(s.charAt(i+1)))  res-=hm.get(s.charAt(i));
+            else    res+=hm.get(s.charAt(i));
         }
 
-        while(!st.isEmpty()){
-            res+=st.pop();
-        }
-
+        res+=hm.get(s.charAt(n-1));
         return res;
     }
 }
