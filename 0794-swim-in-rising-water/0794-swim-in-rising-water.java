b3b0acd3e55a61ffc19f5684ef47aa48e2a579c0
soumya14chomi <68002631+soumya14chomi@users.noreply.github.com>
2025-10-06 15:02:52 -0400

    Time: 20 ms (12.59%), Space: 44.1 MB (96.62%) - LeetHub

---
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int swimInWater(int[][] grid) {
+        PriorityQueue<List<Integer>> pq = new PriorityQueue<>((a, b) -> grid[a.get(0)][a.get(1)] - (grid[b.get(0)][b.get(1)]));
+        int n = grid.length, m = grid[0].length;
+        int[][] minTime = new int[n][m];
+
+        for(int i=0; i<n;i++)   Arrays.fill(minTime[i] , Integer.MAX_VALUE);
+
+        pq.add(List.of(0, 0));
+        minTime[0][0] = grid[0][0];
+
+        int[] rdir = {0, 0, 1, -1}, ldir = {1, -1, 0, 0};
+
+        while(!pq.isEmpty()){
+            List<Integer> top = pq.poll();
+            if(top.get(0) == n-1 && top.get(1) == m-1)  break;
+
+            int i = top.get(0);
+            int j = top.get(1);
+            for(int k=0; k<4; k++){
+                int i1= i+rdir[k], j1 = j+ldir[k];
+                if(isValid(i1, j1, n, m)){
+                    if(minTime[i1][j1] > Math.max(minTime[i][j], grid[i1][j1])){
+                        minTime[i1][j1] = Math.max(minTime[i][j], grid[i1][j1]);
+                        pq.add(List.of(i1, j1));
+                    }
+                }
+            }
+
+        }
+
+         return minTime[n-1][m-1];
+
+
+
+    }
+
+        boolean isValid(int i, int j, int n, int m){
+            return i>=0 && j>=0 && i<n && j<m;
+    }
+}
