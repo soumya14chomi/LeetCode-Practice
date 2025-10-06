09d722abff2a4d1a5ab08f9299fb7608e151d93d
soumya14chomi <68002631+soumya14chomi@users.noreply.github.com>
2025-10-06 15:14:20 -0400

    Time: 13 ms (40.36%), Space: 44.3 MB (73.62%) - LeetHub

--- 0794-swim-in-rising-water/0794-swim-in-rising-water.java
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.java
@@ -2,36 +2,34 @@ class Solution {
     public int swimInWater(int[][] grid) {
 
         int n = grid.length, m = grid[0].length;
-        int[][] minTime = new int[n][m];
+         boolean[][] visited = new boolean[n][m];
 
-                PriorityQueue<List<Integer>> pq = new PriorityQueue<>((a, b) -> minTime[a.get(0)][a.get(1)] - (minTime[b.get(0)][b.get(1)]));
+        PriorityQueue<List<Integer>> pq = new PriorityQueue<>((a, b) -> a.get(2) - b.get(2));
 
-        for(int i=0; i<n;i++)   Arrays.fill(minTime[i] , Integer.MAX_VALUE);
-
-        pq.add(List.of(0, 0));
-        minTime[0][0] = grid[0][0];
+        pq.add(List.of(0, 0, grid[0][0]));
+        // minTime[0][0] = grid[0][0];
+        visited[0][0] = true;
 
         int[] rdir = {0, 0, 1, -1}, ldir = {1, -1, 0, 0};
 
         while(!pq.isEmpty()){
             List<Integer> top = pq.poll();
-            if(top.get(0) == n-1 && top.get(1) == m-1)  break;
+            if(top.get(0) == n-1 && top.get(1) == m-1)  return top.get(2);
 
             int i = top.get(0);
             int j = top.get(1);
+            int h = top.get(2);
             for(int k=0; k<4; k++){
                 int i1= i+rdir[k], j1 = j+ldir[k];
-                if(isValid(i1, j1, n, m)){
-                    if(minTime[i1][j1] > Math.max(minTime[i][j], grid[i1][j1])){
-                        minTime[i1][j1] = Math.max(minTime[i][j], grid[i1][j1]);
-                        pq.add(List.of(i1, j1));
-                    }
+                if(isValid(i1, j1, n, m) && !visited[i1][j1]){
+                    pq.add(List.of(i1, j1, Math.max(grid[i1][j1], h)));
+                    visited[i1][j1] = true;
                 }
             }
 
         }
 
-         return minTime[n-1][m-1];
+         return -1;
 
 
 
