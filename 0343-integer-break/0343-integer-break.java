b560875bbd483bd2a32f5ae184f272496789df7d
soumya14chomi <68002631+soumya14chomi@users.noreply.github.com>
2025-10-07 12:18:45 -0400

    Time: 2 ms (12.23%), Space: 40.7 MB (22.29%) - LeetHub

---
+++ 0343-integer-break/0343-integer-break.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int integerBreak(int n) {
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int i=2; i<=n; i++){
+            dp[i] = 1;
+            for(int j=1; j<i; j++){
+                dp[i] = Math.max(dp[i], Math.max(i-j, dp[i-j])*Math.max(j, dp[j]));
+            }
+
+            // System.out.println(Arrays.toString(dp));
+        }
+
+        return dp[n];
+    }
+}
