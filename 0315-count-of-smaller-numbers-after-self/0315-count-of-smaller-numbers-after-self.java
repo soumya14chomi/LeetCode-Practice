0e129f620158b77f5a03512e193326c7a556b4d4
soumya14chomi <68002631+soumya14chomi@users.noreply.github.com>
2025-10-09 01:24:46 -0400

    Time: 1201 ms (7.03%), Space: 58.5 MB (78.98%) - LeetHub

--- 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.java
+++ 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.java
@@ -1,28 +1,33 @@
 class Solution {
     public List<Integer> countSmaller(int[] nums) {
         int n = nums.length;
-        List<Integer> lst = new ArrayList<>();
+
+        List<Integer> lst = new ArrayList<>(n);
+
         List<Integer> res = new ArrayList<>(Collections.nCopies(n, 0)); // Pre-fill with zeros
 
-        for (int i = n - 1; i >= 0; i--) {
+        for(int i=n-1; i>=0; i--){
+
             int idx = binarySearch(lst, nums[i]);
-            res.set(i, idx); // idx is the count of smaller elements
-            lst.add(idx, nums[i]); // insert in sorted order
+            res.set(i, idx);
+            lst.add(idx, nums[i]);
+                // System.out.println(res);
         }
 
         return res;
     }
 
-    int binarySearch(List<Integer> lst, int num) {
-        int l = 0, r = lst.size();
-        while (l < r) {
-            int mid = l + (r - l) / 2;
-            if (lst.get(mid) < num) {
-                l = mid + 1;
-            } else {
-                r = mid;
-            }
+    int binarySearch(List<Integer> lst, int num ){
+
+        int l = 0, r = lst.size()-1;
+
+        while(l<=r){
+            int mid = (l+r)/2;
+
+            if(lst.get(mid)< num) l=mid+1;
+            else r= mid-1;
         }
+
         return l;
     }
 }
