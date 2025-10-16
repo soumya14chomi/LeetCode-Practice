605da1a2e852320530ca1758cf5d4a5dd9ac47d0
soumya14chomi <68002631+soumya14chomi@users.noreply.github.com>
2025-10-16 15:15:31 -0400

    Time: 41 ms (24.21%), Space: 51.8 MB (34.67%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -3,22 +3,29 @@ class Solution {
 
         List<List<Integer>> res =new ArrayList<>();
 
-        HashSet<String> hs = new HashSet<>();
+        // HashSet<String> hs = new HashSet<>();
         Arrays.sort(nums);
 
         int n= nums.length;
 
-        for(int i=0; i<n; i++){
-            for(int j=i+1, k = n-1; j < k ; ){
+        if(nums[0] == nums[n-1])    {
+            if(nums[0] == 0)   res.add(List.of(0, 0, 0));
+             return res;
+        }
 
-            String str = nums[i]+","+nums[j]+","+nums[k];
-            
-            if(hs.contains(str))
-            {
+        for(int i=0; i<n; i++){
+            if(i>0 && nums[i] == nums[i-1])    continue;
+
+            for(int j=i+1, k = n-1; j < k ; ){
+                
+                if(j> i+1 && nums[j] == nums[j-1])  {
                     j++;
+                    continue;
+                }
+                if(k<n-1 && nums[k] == nums[k+1]){
                     k--;
                     continue;
-            }
+                }
 
                 if(-nums[i] == nums[j] + nums[k]){
 
@@ -26,7 +33,7 @@ class Solution {
                 res.add(List.of(nums[i], nums[j], nums[k]));
                 j++;
                 k--;
-                hs.add(str);
+                // hs.add(str);
 
                 }
 
