19383c8427cca2b5c39f80b5b2e93fedf4bc4d36
soumya14chomi <68002631+soumya14chomi@users.noreply.github.com>
2025-10-19 21:32:40 -0400

    Time: 1 ms (74.53%), Space: 43.2 MB (14.72%) - LeetHub

---
+++ 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int finalValueAfterOperations(String[] arr) {
+        int res = 0;
+
+        for(int i=0; i<arr.length; i++){
+            if(arr[i].equals("++X") || arr[i].equals("X++"))    res++;
+            else    res--;
+        }
+
+        return res;
+    }
+}
