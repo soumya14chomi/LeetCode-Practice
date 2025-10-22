c190843be8081c62505788c8a80aecef66e29081
soumya14chomi <68002631+soumya14chomi@users.noreply.github.com>
2025-10-21 22:13:52 -0400

    Time: 1 ms (99.63%), Space: 41.9 MB (90.63%) - LeetHub

--- 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.java
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.java
@@ -1,7 +1,12 @@
 import java.util.*;
 
 class Solution {
+
+    HashMap<String, List<Integer>> hm = new HashMap<>();
+    
     public List<Integer> diffWaysToCompute(String expression) {
+        if(hm.containsKey(expression))  return hm.get(expression);
+
         List<Integer> res = new ArrayList<>();
         for (int i = 0; i < expression.length(); ++i) {
             char oper = expression.charAt(i);
@@ -10,14 +15,21 @@ class Solution {
                 List<Integer> s2 = diffWaysToCompute(expression.substring(i + 1));
                 for (int a : s1) {
                     for (int b : s2) {
-                        if (oper == '+') res.add(a + b);
-                        else if (oper == '-') res.add(a - b);
-                        else if (oper == '*') res.add(a * b);
+                        int result = 0;
+                        if (oper == '+') result = (a + b);
+                        else if (oper == '-') result = (a - b);
+                        else if (oper == '*') result = (a * b);
+                        res.add(result);
                     }
                 }
             }
         }
-        if (res.isEmpty()) res.add(Integer.parseInt(expression));
+        
+        if (res.isEmpty()) {
+            res.add(Integer.parseInt(expression));
+        }
+
+        hm.put(expression, res);
         return res;
     }
 }
