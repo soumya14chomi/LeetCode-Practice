2adcee230858bb4f3038b7b74dd53f9ab8083b9b
soumya14chomi <68002631+soumya14chomi@users.noreply.github.com>
2025-10-23 22:20:21 -0400

    Time: 0 ms (100%), Space: 44.7 MB (43.04%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -17,27 +17,43 @@ class Solution {
     public Node copyRandomList(Node head) {
 
         if(head == null)    return null;
-        Node newHead = new Node(head.val);
 
-        HashMap<Node, Node> hm = new HashMap<>();
-
-        hm.put(head, newHead);
-
-        Node root = head.next;
-        Node prev = hm.get(head);
-
-        while(root!=null){
-            hm.put(root, new Node(root.val));
-            prev.next = hm.get(root);
-            prev = hm.get(root);
-            root = root.next;
+        Node root = head;
+        while(head!=null){
+            Node newHead = new Node(head.val);
+            newHead.next = head.next;
+            head.next = newHead;
+            head = newHead.next;
         }
 
-        for(Node node: hm.keySet()){
-            hm.get(node).random = hm.get(node.random);
+        head = root;
+        while(head!=null){
+
+            if(head.random == null) head.next.random = null;
+            else
+                head.next.random = head.random.next;
+            head = head.next.next;
         }
 
-        return hm.get(head);
+        head = root;
+        Node root1= root.next;
+        Node head1 = root.next;
+        while(head!=null){
+            Node next = head.next.next;
+            if(head1.next!=null)    
+                head1.next = head1.next.next;
+
+            head.next = next;
+            head1 = head1.next;
+            head = next;
+        }
+        // head = root.next;
+        // while(head!=null){
+        //     System.out.print(head.val+" ->" );
+        //     head = head.next;
+        // }
+
+        return root1;
 
         
     }
