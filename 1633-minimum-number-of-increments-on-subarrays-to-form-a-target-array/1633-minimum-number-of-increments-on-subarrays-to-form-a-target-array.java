8229cd3d42e367c9feceab4c12ec40ff358565ad
soumya14chomi <68002631+soumya14chomi@users.noreply.github.com>
2025-10-29 21:30:14 -0400

    Time: 3 ms (100%), Space: 57 MB (50.7%) - LeetHub

--- 1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array.java
+++ 1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array.java
@@ -1,34 +1,9 @@
 class Solution {
     public int minNumberOperations(int[] target) {
-        Stack<Integer> st = new Stack<>();
-        int n = target.length;
-        int[] left = new int[n];
-
-        for(int i=0; i<n; i++){
-            while(!st.isEmpty() && target[st.peek()] > target[i])  st.pop();
-            if(st.isEmpty())    left[i] = 0;
-            else left[i] = target[st.peek()];
-
-            st.push(i);
+        int min = target[0];
+        for (int i = 1; i < target.length; i++) {
+            min += Math.max(target[i] - target[i - 1], 0);
         }
-
-        int res =0;
-        st.clear();
-
-        for(int i=n-1; i>=0; i--){
-            while(!st.isEmpty() && target[st.peek()] >= target[i])  st.pop();
-
-            if(st.isEmpty())    {
-                res +=(target[i] - left[i]);
-            }
-            else{
-                res+=(target[i] - Math.max(left[i], target[st.peek()]));
-            }
-
-            st.push(i);
-
-        }
-
-        return res;
+        return min;
     }
 }
