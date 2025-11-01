746e7c4ecb7162905571ae5432fba9c38d38ab91
soumya14chomi <68002631+soumya14chomi@users.noreply.github.com>
2025-11-01 16:56:06 -0400

    Time: 209 ms (7.01%), Space: 127.1 MB (5.52%) - LeetHub

---
+++ 0220-contains-duplicate-iii/0220-contains-duplicate-iii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+    if (k <= 0 || t < 0) return false;
+
+    TreeSet<Long> set = new TreeSet<>();
+    for (int i = 0; i < nums.length; i++) {
+        Long floor = set.floor((long) nums[i] + t);
+        Long ceil = set.ceiling((long) nums[i] - t);
+        if ((floor != null && floor >= nums[i]) || (ceil != null && ceil <= nums[i])) {
+            return true;
+        }
+
+        set.add((long) nums[i]);
+        if (i >= k) set.remove((long) nums[i - k]);
+    }
+    return false;
+}
+
+}
