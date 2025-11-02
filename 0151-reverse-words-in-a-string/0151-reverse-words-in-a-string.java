0ed7d1a2f0be1d45dcb4377be310805807ed3e39
soumya14chomi <68002631+soumya14chomi@users.noreply.github.com>
2025-11-01 22:34:23 -0400

    Time: 9 ms (24.52%), Space: 47 MB (6.8%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -0,0 +1,24 @@
+// import org.apache.commons.lang3.StringUtils;
+
+class Solution {
+    public String reverseWords(String s) {
+        s= s.stripLeading();
+        s = s.stripTrailing();
+        String[] strs = s.split(" ");
+        s = "";
+        for(int i=strs.length-1; i>=0; i--){
+            // System.out.println(isNotBlank(strs[i]));
+            if(isNotBlank(strs[i]))  {
+                s+=strs[i].trim();
+                if(i!=0)    s+=" ";
+            }  
+            
+        }
+
+        return s;
+    }
+
+    public static boolean isNotBlank(String str) {
+        return str != null && str.length()>0 && !str.trim().isEmpty();
+    }
+}
