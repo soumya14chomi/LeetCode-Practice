4344d8d4b5e1b6add24b4dd3cb71c1ec4d6f7537
soumya14chomi <68002631+soumya14chomi@users.noreply.github.com>
2026-01-12 23:42:57 -0500

    Time: 1 ms (95.11%), Space: 44.9 MB (49.54%) - LeetHub

--- 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.java
+++ 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.java
@@ -3,13 +3,10 @@ class Solution {
         int ans = 0;
 
         for(int i=1; i<points.length; i++){
-            int diagTime = Math.min(Math.abs(points[i][0] - points[i-1][0]),
-                                        Math.abs(points[i][1] - points[i-1][1]));
-
-            // System.out.println(diagTime);
+            
 
 
-            ans+= ( Math.abs(points[i][0] - points[i-1][0]) + Math.abs(points[i][1] - points[i-1][1]) - diagTime);
+            ans+= Math.max(Math.abs(points[i][0] - points[i-1][0]), Math.abs(points[i][1] - points[i-1][1]));
         }
 
         return ans;
